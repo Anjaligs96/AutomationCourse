412e70dcd12a82384f074df355289b316ca4af5e
anjal <anjal@Anjali>
2025-07-29 20:40:54 +0530

    commit

--- AutomationCourse/src/test/java/testScript/LoginTest.java
+++ AutomationCourse/src/test/java/testScript/LoginTest.java
@@ -4,52 +4,48 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
+import pages.LoginPage;
 import seleniumBasics.TestNGBase;
 
-public class LoginTest  extends TestNGBase{
-	
+public class LoginTest extends TestNGBase {
+
 	@Test
-	public void verifyUserLoginWithValidCredentials()
-	{
-		WebElement username=driver.findElement(By.xpath("//input[@id='user-name']"));
+	public void verifyUserLoginWithValidCredentials() {
+		LoginPage loginpage = new LoginPage(driver);
+		loginpage.enterUserNameOnUserNameField();
+		loginpage.enterPasswordOnPasswordField();
+		loginpage.clickOnSignInButton();
+
+	}
+
+	@Test
+	public void verifyUserLoginWithValidUsernameAndInvalidPassword() {
+
+		WebElement username = driver.findElement(By.xpath("//input[@id='user-name']"));
 		username.sendKeys("standard_user");
-		WebElement password= driver.findElement(By.xpath("//input[@id='password']"));
-		password.sendKeys("secret_sauce");
-		WebElement loginbutton= driver.findElement(By.xpath("//input[@id='login-button']"));
-		loginbutton.click();
-	}
-	
-   @Test
-	public void verifyUserLoginWithValidUsernameAndInvalidPassword()
-	{
-	   
-		WebElement username=driver.findElement(By.xpath("//input[@id='user-name']"));
-		username.sendKeys("standard_user");
-		WebElement password= driver.findElement(By.xpath("//input[@id='password']"));
+		WebElement password = driver.findElement(By.xpath("//input[@id='password']"));
 		password.sendKeys("password");
-		WebElement loginbutton= driver.findElement(By.xpath("//input[@id='login-button']"));
+		WebElement loginbutton = driver.findElement(By.xpath("//input[@id='login-button']"));
 		loginbutton.click();
 	}
-   
-   @Test
-	public void verifyUserLoginWithInvalidUsernameAndValidPassword()
-	{
-		WebElement username=driver.findElement(By.xpath("//input[@id='user-name']"));
+
+	@Test
+	public void verifyUserLoginWithInvalidUsernameAndValidPassword() {
+		WebElement username = driver.findElement(By.xpath("//input[@id='user-name']"));
 		username.sendKeys("user");
-		WebElement password= driver.findElement(By.xpath("//input[@id='password']"));
+		WebElement password = driver.findElement(By.xpath("//input[@id='password']"));
 		password.sendKeys("secret_sauce");
-		WebElement loginbutton= driver.findElement(By.xpath("//input[@id='login-button']"));
+		WebElement loginbutton = driver.findElement(By.xpath("//input[@id='login-button']"));
 		loginbutton.click();
 	}
-   
-   @Test
-	public void verifyUserLoginWithInValidCredentials()
-	{
-		WebElement username=driver.findElement(By.xpath("//input[@id='user-name']"));
+
+	@Test
+	public void verifyUserLoginWithInValidCredentials() {
+		WebElement username = driver.findElement(By.xpath("//input[@id='user-name']"));
 		username.sendKeys("user");
-		WebElement password= driver.findElement(By.xpath("//input[@id='password']"));
+		WebElement password = driver.findElement(By.xpath("//input[@id='password']"));
 		password.sendKeys("password");
-		WebElement loginbutton= driver.findElement(By.xpath("//input[@id='login-button']"));
+		WebElement loginbutton = driver.findElement(By.xpath("//input[@id='login-button']"));
 		loginbutton.click();
 	}
 }
